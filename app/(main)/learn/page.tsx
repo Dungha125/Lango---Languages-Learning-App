8456331981c8d9576b98461046dec8eb291d5e7e
Dungha125 <dungha122405@gmail.com>
2024-05-04 01:48:36 +0700

    dungha

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,17 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/ui/sticky-wrapper";
+import { Federant } from "next/font/google";
+import { Header } from "./header";
+
 const LearnPage = () => {
     return(
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                My Sticky sidebar
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Vietnamese" />
+            </FeedWrapper>
         </div>
     );
 }
